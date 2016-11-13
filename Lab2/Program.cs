987bfbfbaf952bffc4f4de9881432055a115d4d8
olegb <olegb@DESKTOP-9QLPB7I>
2016-11-13 20:13:42 +0700

    Add project files.

---
+++ Lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = File.ReadAllText("input.txt");
+
+            var n = Convert.ToInt32(file);
+
+            var result = (n * (n + 1) / 2) + 1;
+
+            File.WriteAllText("output.txt", result.ToString());
+        }
+    }
+}
